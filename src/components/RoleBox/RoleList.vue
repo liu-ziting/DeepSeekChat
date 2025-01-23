57f04d7ce98b170e25a42d0a0977cd5ec8aef4b7
liuziting <57311725+liu-ziting@users.noreply.github.com>
2025-01-23 19:00:02 +0800

    Update RoleList.vue

--- src/components/RoleBox/RoleList.vue
+++ src/components/RoleBox/RoleList.vue
@@ -94,7 +94,7 @@ export default {
             ],
             isThinking: false,
             mode: 'normal',
-            model: 'aliyuncs',
+            model: 'deepseek',
             tab: 'chat',
             selectedPrompt: null
         }
