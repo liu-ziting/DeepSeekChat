ac158d33ccf7aa5f962d86f322e1ac81724fa510
liuziting <57311725+liu-ziting@users.noreply.github.com>
2025-01-24 22:13:43 +0800

    Update MessageBox.vue

--- src/components/ChatBox/MessageBox.vue
+++ src/components/ChatBox/MessageBox.vue
@@ -71,7 +71,7 @@
                 </div>
             </div>
         </div>
-        <p class="w-full ml-[52px] text-xs text-gray-500 mt-1" v-if="message.token" style="width: 60%">
+        <p class="w-full ml-[52px] text-xs text-gray-500 mt-1" v-if="message.token" style="width: 60%;overflow: hidden;s">
             <span style="float: left">{{ message.duration }}s</span>
             <span style="background: #888d92; display: block; height: 8px; margin: 4px 8px; width: 1px; float: left"></span>
             <span style="float: left">{{ message.token }}Token</span>
